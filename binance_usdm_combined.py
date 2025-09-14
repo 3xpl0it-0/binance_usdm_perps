b97a9d243f78779fb129e525d280a0c8f50bedea
3xpl0it <3xpl0it.0@gmail.com>
2025-09-14 02:22:51 +0100

    Update binance_usdm_combined.py
    improving readability

--- binance_usdm_combined.py
+++ binance_usdm_combined.py
@@ -1,11 +1,6 @@
-import requests
-import pandas as pd
-import numpy as np
-import time
-import requests
-import matplotlib.pyplot as plt
+"""combine tohlcv and funding and save to csv"""
+
 import intro
-from binance_usdm_asset import listing_df
 from binance_usdm_ohlcv import df as datadf
 from binance_usdm_funding import fundingdf
 
@@ -14,29 +9,24 @@ comdf['funding'] = None
 updates = {}
 
 for asset in comdf['asset'].unique():
-    
+
     tempdatadf = datadf[datadf['asset'] == asset].reset_index(drop = False)
     tempfundingdf = fundingdf[fundingdf['asset'] == asset].reset_index(drop = False)
-    
+
     for index in tempdatadf.index[1:]:
-        
+
         oldtime = tempdatadf['timestamp'].iloc[index-1]
         newtime = tempdatadf['timestamp'].iloc[index]
-        
-        fundingperiod = tempfundingdf[(tempfundingdf['timestamp'] > oldtime) & (tempfundingdf['timestamp'] <= newtime)]
+
+        fundingperiod = tempfundingdf[(
+            tempfundingdf['timestamp'] > oldtime) & (tempfundingdf['timestamp'] <= newtime
+        )]
         _fundingperiod = fundingperiod['funding'].astype(float)
         funding = sum(_fundingperiod)
-        
+
         updates[tempdatadf['index'].iloc[index-1]] = funding
-        
+
 comdf.loc[list(updates.keys()), 'funding'] = list(updates.values())
-        
-print('----------- saving binance_ohlcvf_data.csv -----------')
+
+print('saving binance_ohlcvf_data.csv')
 comdf.to_csv("binance_ohlcvf_data.csv", index=False)
-        
-        
-        
-        
-        
-        
-        

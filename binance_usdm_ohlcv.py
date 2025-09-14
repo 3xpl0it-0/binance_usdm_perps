d74129bce3e299a0e521f71e7e11c324f496285e
3xpl0it <3xpl0it.0@gmail.com>
2025-09-14 02:25:29 +0100

    Update binance_usdm_ohlcv.py
    improving readability

--- binance_usdm_ohlcv.py
+++ binance_usdm_ohlcv.py
@@ -1,123 +1,126 @@
-import requests
-import pandas as pd
-import numpy as np
+"""importing tohlcv data into dataframe and csv file (csv saved if script ran as main)"""
+
 import time
+
+import pandas as pd
 import requests
-import matplotlib.pyplot as plt
+
 from binance_usdm_asset import listing_df
 
 if __name__ == "__main__":
-    import info
+    import intro
 
-BINANCE_BASE_URL = 'https://fapi.binance.com'
-BINANCE_KLINE_URL = '/fapi/v1/klines'
+BINANCE_BASE_URL = "https://fapi.binance.com"
+BINANCE_KLINE_URL = "/fapi/v1/klines"
+
 
 def get_kline(
     listing_data: list | None = None,
     base_url: str | None = None,
-    kline_url: str | None = None
+    kline_url: str | None = None,
 ) -> list[tuple[str, int, int, int, int, int, int, int]]:
-    
+    """get candlestick data for tickers given"""
+
     if not listing_data:
         _listing_data = ASSET_LISTING
     else:
         _listing_data = listing_data
-    
+
     if not base_url:
         _base_url = BINANCE_BASE_URL
     else:
         _base_url = base_url
-        
+
     if not kline_url:
         _kline_url = BINANCE_KLINE_URL
     else:
         _kline_url = kline_url
-    
+
     kline = []
-    url = f'{_base_url}{_kline_url}'
-    
+    url = f"{_base_url}{_kline_url}"
+
     for asset in _listing_data:
         start = asset[1]
         end = asset[2]
-        
+
         try:
             while start < end:
-                
                 params = {
                     "symbol": asset[0],
                     "startTime": start,
                     "limit": 1000,
-                    "interval": "1d"
+                    "interval": "1d",
                 }
-                
-                response = requests.get(url, params=params)    
+
+                response = requests.get(url, params=params, timeout=10)
                 data = response.json()
-                
+
                 for i in data[:-1]:
-                    kline.append((asset[0], i[0], i[1], i[2], i[3], i[4], i[7])) # open time, open, high, low, close, volume
-                    
+                    kline.append((asset[0], i[0], i[1], i[2], i[3], i[4], i[7]))
+                    # open time, open, high, low, close, volume
+
                 start = data[-1][0]
-                
-                time.sleep(.5)
-                    
+
+                time.sleep(0.5)
+
         except requests.exceptions.RequestException as e:
             print(f"Warning: API request failed for {asset[0]}: {e}")
-            print('because of this returning blank list')
-            
+            print("because of this returning blank list")
+
             return []
-    
-        print(f'{asset} kline saved')
-    
+
+        print(f"{asset} kline saved")
+
     return kline
+
 
 ASSET_LISTING = listing_df.values.tolist()
 
-print('----------- getting kline data -----------')
-print('')
+print("getting kline data\n")
 
 asset_data = get_kline()
 
-print('')
-print('----------- finished getting kline data -----------')
-print('')
+print("\nfinished getting kline data\n")
 
 columns = ["asset", "timestamp", "open", "high", "low", "close", "volume"]
 df = pd.DataFrame(asset_data, columns=columns)
 
-print('----------- checking for missing entries -----------')
-print('')
+print("checking for missing entries\n")
 
 for index in df.index[1:]:
-    
-    if df['timestamp'].iloc[index] - df['timestamp'].iloc[index-1] != 86400000 and df['asset'].iloc[index] == df['asset'].iloc[index-1]:
-        print(f'problem: {df["timestamp"].iloc[index] - df["timestamp"].iloc[index-1]}')
-        
-print('----------- finished checking for missing entries -----------')
-print('')
+    if (
+        df["timestamp"].iloc[index] - df["timestamp"].iloc[index - 1] != 86400000
+        and df["asset"].iloc[index] == df["asset"].iloc[index - 1]
+    ):
+        print(f"problem: {df['timestamp'].iloc[index] - df['timestamp'].iloc[index - 1]}")
 
-print('----------- checking for discrepencies in open - close -----------')
-print('')
+print("finished checking for missing entries\n")
+
+print("checking for discrepencies in open - close\n")
+
 for index in df.index[1:]:
-    
-    open_ = float(df['open'].iloc[index])
-    prev_close = float(df['close'].iloc[index-1])
-    
-    if open_ - prev_close > open_*0.005 and df['asset'].iloc[index] == df['asset'].iloc[index-1]:
-        print(f'problem: (time: {df["timestamp"].iloc[index]} - {(open_ - prev_close)/prev_close}')
-        print(f'asset: {df["asset"].iloc[index]} open: {open_} prev close: {prev_close}')
+    open_ = float(df["open"].iloc[index])
+    prev_close = float(df["close"].iloc[index - 1])
 
-print('----------- finished checking for discrepencies in open - close -----------')
-print('')
+    if (
+        open_ - prev_close > open_ * 0.005
+        and df["asset"].iloc[index] == df["asset"].iloc[index - 1]
+    ):
+        print(
+            f"problem: (time: {df['timestamp'].iloc[index]} - {(open_ - prev_close) / prev_close}"
+        )
+        print(
+            f"asset: {df['asset'].iloc[index]} open: {open_} prev close: {prev_close}"
+        )
 
-print('----------- checking for 0/Nan values -----------')
-print('')
+print("finished checking for discrepencies in open - close\n")
+
+print("checking for 0/Nan values\n")
 
 print(df.isna().any())
 
-print('----------- finished checking for 0/Nan values -----------')
-print('')
+print("finished checking for 0/Nan values\n")
 
 if __name__ == "__main__":
-
-    print('----------- saving binance_data.csv -----------')
+    print("saving binance_data.csv")
     df.to_csv("binance_data.csv", index=False)
